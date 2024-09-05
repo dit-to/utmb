82d1b1266a5c3f0cb15dbd053a6d99bc3935aa90
이은지 <eunjilee@Mac-mini.local>
2024-09-05 19:00:45 +0900

    Set Font

--- src/components/Main.js
+++ src/components/Main.js
@@ -17,25 +17,29 @@ const Main = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-white">
-      <h1 className="text-3xl font-bold text-purple-600 mb-4">무료 UTM 빌더</h1>
-      <p className="text-gray-600 mb-6">잠재고객이 어떤 경로로 유입되었는지 확인해보세요!</p>
-      <input 
-        type="url"
-        value={url}
-        onChange={handleInputChange}
-        onFocus={() => !url && setUrl('https://')}
-        placeholder="https://example.com"
-        className="border border-gray-300 p-2 rounded-md w-64 mb-2 text-center"
-      />
-      <p className="text-sm text-gray-500 mb-4">유입경로를 분석할 웹사이트 주소를 입력해주세요.</p>
-      <button 
-        onClick={handleNextClick}
-        className={`py-2 px-6 rounded-md font-semibold ${url.includes('.') ? 'bg-purple-600 text-white' : 'bg-gray-300 text-gray-500 cursor-not-allowed'}`}
-        disabled={!url.includes('.')}
-      >
-        다음
-      </button>
+    <div className="flex items-center justify-center h-screen bg-white">
+      <div className="h-full w-full max-w-md flex flex-col items-center">
+        <div className='font-title font-900'>
+          <h1 className="text-3xl  text-primary-500 mb-4">무료 UTM 빌더</h1>
+        </div>
+        <p className="text-gray-600 mb-6">잠재고객이 어떤 경로로 유입되었는지 확인해보세요!</p>
+        <input 
+          type="url"
+          value={url}
+          onChange={handleInputChange}
+          onFocus={() => !url && setUrl('https://')}
+          placeholder="https://example.com"
+          className="border border-gray-300 p-2 rounded-md w-64 mb-2 text-center"
+        />
+        <p className="text-sm text-gray-500 mb-4">유입경로를 분석할 웹사이트 주소를 입력해주세요.</p>
+        <button 
+          onClick={handleNextClick}
+          className={`py-2 px-6 rounded-md font-semibold ${url.includes('.') ? 'bg-purple-600 text-white' : 'bg-gray-300 text-gray-500 cursor-not-allowed'}`}
+          disabled={!url.includes('.')}
+        >
+          다음
+        </button>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,19 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        title: ['yg-jalnan', 'sans-serif'], // 타이틀 폰트 패밀리 설정
+        title: ['Paperlogy', 'sans-serif'], // 타이틀 폰트 패밀리 설정
         body: ['Pretendard', 'sans-serif'], // 본문 폰트 패밀리 설정
+      },
+      fontWeight: {
+        100: '100',
+        200: '200',
+        300: '300',
+        400: '400',
+        500: '500',
+        600: '600',
+        700: '700',
+        800: '800',
+        900: '900',
       },
       colors: {
         primary: {

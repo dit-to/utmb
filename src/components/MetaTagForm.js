37622ae09a3c34d9e7c54f067ea2cd9abcd96738
이은지 <eunjilee@Mac-mini.local>
2024-09-10 15:26:22 +0900

    Updated of MetaBuild for Mobile

--- src/components/MetaTagForm.js
+++ src/components/MetaTagForm.js
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 
 const MetaTagForm = () => {
   return (
-    <div className='flex items-center justify-center text-[80px] mt-[200px] font-title font-500 text-gray-500'>뚝딱뚝딱..🛠️</div>
+    <div className='flex items-center justify-center text-[40px] md:text-[80px] mt-[200px] font-title font-500 text-gray-500'>뚝딱뚝딱..🛠️</div>
   );
 };
 

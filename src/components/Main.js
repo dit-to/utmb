8689340d86b9f03d2e986abbff01b4b84a4fde74
이은지 <eunjilee@Mac-mini.local>
2024-09-10 14:57:53 +0900

    Updated Design of Main

--- src/components/Main.js
+++ src/components/Main.js
@@ -30,8 +30,10 @@ const Main = () => {
     <div className="flex w-screen items-center justify-center h-screen bg-white main-frame">
       <div className="h-full w-full max-w-lg flex flex-col items-center px-5">
         <div className='flex flex-col font-title mt-[160px] items-center'>
-          <h1 className="text-5xl font-700 text-primary-500 mb-6">무료 UTM 빌더</h1>
-          <h2 className="text-xl font-500 text-gray-500 mb-8">잠재고객이 어떤 경로로 유입되었는지 확인해보세요!</h2>
+          <h1 className="text-4xl md:text-5xl font-700 text-primary-500 mb-6">무료 UTM 빌더</h1>
+          <h2 className="text-lg md:text-xl font-500 text-gray-500 mb-8 text-center">
+            잠재고객이 어떤 경로로<br className="md:hidden" /> 유입되었는지 확인해보세요!
+          </h2>
           <div className='w-full font-title flex flex-col gap-7'>
             <input 
               type="url"
@@ -39,11 +41,11 @@ const Main = () => {
               onChange={handleInputChange}
               onFocus={() => !url && setUrl('https://')}
               placeholder="https://example.com"
-              className="bg-primary-50 p-4 px-4 rounded-xl w-full text-gray-600 placeholder-gray-400 main-input focus:shadow-[0_0_20px_0_rgba(97,95,225,0.3)] focus:outline-[1px] focus:outline-primary-500"
+              className="bg-primary-50 p-3 md:p-4 rounded-xl w-full text-gray-600 placeholder-gray-400 main-input focus:shadow-[0_0_20px_0_rgba(97,95,225,0.3)] focus:outline-[1px] focus:outline-primary-500"
             />
             <button 
               onClick={handleNextClick}
-              className={`h-[60px] px-6 rounded-full text-xl w-full font-400 ${url.includes('.') ? 'bg-primary-500 text-white font-600' : 'bg-gray-300 text-gray-400 cursor-not-allowed'}`}
+              className={`h-[50px] md:h-[60px] px-6 rounded-full text-base md:text-xl w-full font-400 ${url.includes('.') ? 'bg-primary-500 text-white font-600' : 'bg-gray-300 text-gray-400 cursor-not-allowed'}`}
               disabled={!url.includes('.')}
             >
               {buttonLabel}
